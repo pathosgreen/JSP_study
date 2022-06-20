25f9f31cb410be7fc9583877260a58411d8ade24
DESKTOP-P2E4GPS\505 <hexun_@naver.com>
2022-06-20 16:49:21 +0900

    2022_06_20 JSP 수업

---
+++ chap02/src/main/java/controller/RadioServlet.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RadioServlet
+ */
+@WebServlet("/RadioServlet")
+public class RadioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=utf-8");
+		
+		String gender = request.getParameter("gender");
+		String chk_mail = request.getParameter("chk_mail");
+		String content = request.getParameter("content");
+		
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("당신이 입력한 정보입니다.<hr>");
+		out.print("성별 : <b>");
+		out.print(gender);
+		out.print("</b><br>메일 정보 수신 여부 : <b>");
+		out.print(chk_mail);
+		out.print("</b><br>가입 인사 : <b><pre>");
+		out.print(content);
+		out.print("</b></pre><a href='javascript:history.go(-1)'>다시</a>");
+		out.print("</body></html>");
+		
+		out.close();
+	}
+
+}
